ea4946579591def02fdb3584da57385a3ef5c020
jm95aguilar <jm95aguilar@hotmail.com>
2015-06-05 19:00:43 -0600

    Manejo de Excepciones, Función insertar_usuario

--- scripts/insertar_usuario.sql
+++ scripts/insertar_usuario.sql
@@ -7,12 +7,15 @@ CREATE DEFINER=`WingWatch`@`localhost` PROCEDURE `insertar_usuario`(IN `NICKNAME
 	COMMENT ''
 BEGIN
 DECLARE EXIT HANDLER FOR SQLEXCEPTION 
-SELECT 'SQLException invoked';
+BEGIN
+	SELECT 'SQLException invoked';
+	ROLLBACK;
+END;
 START TRANSACTION;
-INSERT INTO wingwatch.persona (persona.NOMBRE,persona.PRIMER_APELLIDO,persona.SEGUNDO_APELLIDO,persona.CEDULA) VALUES (`NOMBRE`,`P_APELLIDO`,`S_APELLIDO`,`CEDULA`);
-SET @last_id = LAST_INSERT_ID(); 
-INSERT INTO wingwatch.telefono (telefono.TELEFONO,telefono.persona_ID) VALUES (`TELEFONO`,@last_id);
-INSERT INTO wingwatch.correo (correo.CORREO,correo.persona_ID) VALUES (`CORREO`,@last_id);
-INSERT INTO wingwatch.usuario (usuario.NICKNAME,usuario.PASSWORD,usuario.CEDULA,usuario.ES_ADMIN,usuario.FOTO_PERFIL,usuario.tipo_usuario_ID,usuario.persona_ID) VALUES (`NICKNAME`,`PASSWORD`,`CEDULA`,`ES_ADMIN`,`FOTO_PERFIL`,`TIPO`,@last_id);
-COMMIT;
+	INSERT INTO wingwatch.persona (persona.NOMBRE,persona.PRIMER_APELLIDO,persona.SEGUNDO_APELLIDO,persona.CEDULA) VALUES (`NOMBRE`,`P_APELLIDO`,`S_APELLIDO`,`CEDULA`);
+	SET @last_id = LAST_INSERT_ID(); 
+	INSERT INTO wingwatch.telefono (telefono.TELEFONO,telefono.persona_ID) VALUES (`TELEFONO`,@last_id);
+	INSERT INTO wingwatch.correo (correo.CORREO,correo.persona_ID) VALUES (`CORREO`,@last_id);
+	INSERT INTO wingwatch.usuario (usuario.NICKNAME,usuario.PASSWORD,usuario.CEDULA,usuario.ES_ADMIN,usuario.FOTO_PERFIL,usuario.tipo_usuario_ID,usuario.persona_ID) VALUES (`NICKNAME`,`PASSWORD`,`CEDULA`,`ES_ADMIN`,`FOTO_PERFIL`,`TIPO`,@last_id);
+	COMMIT;
 END //
